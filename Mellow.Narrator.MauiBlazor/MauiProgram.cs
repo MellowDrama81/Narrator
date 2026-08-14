6c3f1a6d36da887922212529d5a65ccf52e97c17
BrianMarshall81 <Brian@Longbourn.email>
2026-08-14 11:20:15 +0800

    Fix Hybrid Android platform requirements

--- Mellow.Narrator.MauiBlazor/MauiProgram.cs
+++ Mellow.Narrator.MauiBlazor/MauiProgram.cs
@@ -14,7 +14,7 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder.UseMauiApp<App>();
         builder.Services.AddMauiBlazorWebView();
-#if DEBUG
+#if DEBUG && WINDOWS
         builder.Services.AddBlazorWebViewDeveloperTools();
 #endif
         builder.Services
